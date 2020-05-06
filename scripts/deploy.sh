976640ad4bbb5055d2fb5ad9e3cc7dc99e80b71b
Romain Rochegude <romain.rochegude@gmail.com>
2020-05-07 01:24:47 +0200

    configure circleci deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "We are going to publish version $CIRCLE_SHA1"
 echo $GPG_KEY_CONTENTS | base64 -d > /tmp/secret.gpg
-./gradlew -PnewVersion="$VERSION" clean jar publish closeAndReleaseRepository
+./gradlew -PnewVersion="$CIRCLE_SHA1" clean jar publish closeAndReleaseRepository
