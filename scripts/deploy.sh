6e6644d3a55d83134d084652f2cea34f11cc8115
Romain Rochegude <romain.rochegude@gmail.com>
2020-05-06 23:48:26 +0200

    configure travis deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "$GPG_KEY_CONTENTS" | base64 -d > /secret.gpg
+echo "${$GPG_KEY_CONTENTS}" | base64 --decode >/tmp/secret.gpg
 
 ./gradlew -PnewVersion="$TRAVIS_TAG" clean jar publish closeAndReleaseRepository
